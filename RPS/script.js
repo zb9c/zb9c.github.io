98d160b649aedff2c909d04ea552b6a795f930ad
zb9c <bryan.bush056@topper.wku.edu>
2019-05-02 16:45:07 -0500

    Add files via upload
    updated jjavascript

--- RPS/script.js
+++ RPS/script.js
@@ -9,44 +9,65 @@ setTimeout(
 );
 
 
-
 let userChoice;
 let botChoice;
 var choices = ["rock", "paper", "scissors"];
+let userScore = 0;
+let computerScore = 0;
 
 
+botChoice = assignBot();
+console.log(botChoice);
+
+function assignBot(){
+	x = Math.floor(Math.random() * (3 - 0) + 0);
+	return choices[x];
+}
 function clickRock(){
-	userChoice = 0;
+	userChoice = "rock";
+	alert(compare(userChoice, botChoice));
 }
 function clickPaper(){
-	userChoice = 1;
+	userChoice = "paper";
+ 	alert(compare(userChoice, botChoice));
 }
 function clickScissors(){
-	userChoice = 2;
+	userChoice = "scissors";
+	alert(compare(userChoice, botChoice));
 }
-function assignBot(){
-	botChoice = Math.floor(Math.random() * (3 - 0) + 0);
-	console.log(botChoice);
-}
+
 function compare(choice1, choice2) {
     choice1 = choices.indexOf(choice1);
     choice2 = choices.indexOf(choice2);
-    
     if (choice1 == choice2) {
         return "Tie";
     }
     if (choice1 == choices.length - 1 && choice2 == 0) {
-        return "Right wins";
+        computerScore = computerScore + 1;
+	document.getElementById("computer-score").innerHTML = computerScore;
+	return "Computer Wins!";	
     }
     if (choice2 == choices.length - 1 && choice1 == 0) {
-        return "Left wins";
-    }
+        userScore = userScore + 1;
+	document.getElementById("user-score").innerHTML = userScore;
+	return "You Win!";
+	    }
     if (choice1 > choice2) {
-        return "Left wins";
+        userScore = userScore + 1;
+	document.getElementById("user-score").innerHTML = userScore;
+	return "You Win!";	
     } else {
-        return "Right wins";
+	computerScore = computerScore + 1;
+	document.getElementById("computer-score").innerHTML = computerScore;
+        return "Computer Wins";	
     }
+    botChoice = assignBot();
+    console.log(botChoice);
 }
-
-
-botChoice = assignBot();
+function resetGame(){
+	let userScore = 0;
+	let computerScore = 0;
+	botChoice = assignBot();
+	document.getElementById("computer-score").innerHTML = computerScore;
+	document.getElementById("user-score").innerHTML = userScore;
+}
